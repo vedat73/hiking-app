d6f981b5543c0d214ac07cc622d85041f97ac271
vedat73 <vedat.ozlu@outlook.com>
2023-07-15 11:03:50 +0300

    Initial Commit

---
+++ hiking/hikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  hikingApp.swift
+//  hiking
+//
+//  Created by Vedat Ozlu on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct hikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
